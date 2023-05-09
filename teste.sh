4ec86fb5582c8e515d3a81165f0336cc44661c35
lirialmodobar <liriel.almodobar@gmail.com>
2023-05-09 16:40:08 -0300

    added ids, added unk label, sorted positions (start in ascending order, end in descending order), filtered to keep only the fragment with highest range in fragments that start at the same place

--- teste.sh
+++ teste.sh
@@ -2,9 +2,10 @@ INPUT_FILES=/home/santoro-lab/teste_dir/simulados
 WD=/home/santoro-lab/teste_dir #sets the working directory
 ls $INPUT_FILES  > $WD/collapse_results.txt
 while read collapse; do
-	cat $INPUT_FILES/$collapse >> $WD/all_anc_all_chr.txt
+	FILE_NAME=$(echo $collapse | head -n 1)
+	cut -f1-4 $INPUT_FILES/$collapse | cat | sed "s/^/$FILE_NAME\t/"  >> $WD/all_anc_all_chr.txt
 done < $WD/collapse_results.txt
-labels=("NAT" "EUR" "AFR")
+labels=("NAT" "EUR" "AFR" "UNK")
 for label in "${labels[@]}"; do
 	label_lower="${label,,}"
 	if [ ! -d $WD/$label_lower ]; then
@@ -12,6 +13,9 @@ for label in "${labels[@]}"; do
 	fi
     	grep -w "$label" $WD/all_anc_all_chr.txt > $WD/$label_lower/"$label_lower"_all.txt
 	for chr in {1..8}; do
-		awk -v chr=$chr '{ if ($1 == chr) {print}}' $WD/$label_lower/"$label_lower"_all.txt | sort -n -k 2 > $WD/$label_lower/chr_"$chr"_"$label_lower"_sorted.txt
+		OUTPUT=$(awk -v chr=$chr '{ if ($2 == chr) {print}}' $WD/$label_lower/"$label_lower"_all.txt | sort -n -k 3,3 -k 4,4r | awk '!seen[$3]++')
+		if [ -n "$OUTPUT" ]; then
+        	echo "$OUTPUT" > $WD/$label_lower/chr_"${chr}"_"${label_lower}"_sorted.txt
+    		fi
 	done
 done
