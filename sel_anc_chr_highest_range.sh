720782259acb2c8ef67725fac1ba5cb872f2f7fe
lirialmodobar <liriel.almodobar@gmail.com>
2023-05-10 18:15:54 -0300

    updated the script so it has a column that tells if there's a gap between fragments (1 = true, 0 = false)

--- sel_anc_chr_highest_range.sh
+++ sel_anc_chr_highest_range.sh
@@ -15,8 +15,14 @@ for label in "${labels[@]}"; do
 	for chr in {1..22}; do
 		CHRS=$(awk -v chr=$chr '{ if ($2 == chr) {print}}' $WD/$label_lower/"$label_lower"_all.txt | sort -n -k 3,3 -k 4,4r | awk '!seen[$3]++')
 		if [ -n "$CHRS" ]; then
-        	echo "$CHRS" | awk '{print $1, $2, $3, $4, ($4-$3)/1000, $5}' > $WD/$label_lower/chr_"${chr}"_"${label_lower}"_sort_filt_size.txt
+        	echo "$CHRS" | awk 'NR==1 {a=$4; print $1, $2, $3, $4, ($4-$3)/1000, "NA", $5; next} {print $1, $2, $3, $4,($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' > $WD/$label_lower/chr_"${chr}"_"${label_lower}"_sort_filt_size.txt
     		fi
 	done
 done
 rm $WD/collapse_results.txt
+#por chr: verificar se tem gap (checa se pos final da coluna x menor ou igual a pos inicial da outra, criar coluna gap e o valor do gap) - for chr 
+#verificar: pos inicial e final de cada cromossomo (api e array? hg!), ai por anc o quao distante o inicio ta do inicio real e o do fim ta do fim real por anc
+##e cobertura entre tds as ancs? tipo menor pos entre tds as ancs e maior pos entre tds as ancs (all_chr_all_anc) e ver quao dist ta, se inc unk vai ser chr td ue...
+###mas se for sem unk, pode ser interessante ver o qto conseguimos cobrir de anc atribuida 
+#verificar maior fragmento unknown (das outras anc tbm?)
+
