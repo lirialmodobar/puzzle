cdfe3c2a9eea817fa77e76277eb567cc991a45ed
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-05-18 21:49:10 +0000

    r script to plot the fragments agains their respective chr

--- chr_graph.R
+++ chr_graph.R
@@ -6,15 +6,17 @@ library(karyoploteR)
 
 # Read the data file
 dados <- read.table("nat/chr_1_nat_sort_filt_size_gap.txt", header = FALSE, sep=' ')
+positions <- dados[2:4]
+positions[,1] <- paste0("chr", positions[,1])
 
 # Extract the chromosome from your data (assuming all values in the second column are the same)
-chromosome <- dados[1, 2]
+chromosome <- positions[1, 1]
 
+pdf("karyotype_plot.pdf")
 # Create a karyotype object for chromosome drawing with the extracted chromosome
 kp <- plotKaryotype(genome = "hg38", chromosomes = chromosome)
 
 # Add genomic regions of the specified chromosome to the plot
-kpPlotRegions(kp, data = dados, start.column = 3, end.column = 4, col = "blue", chr.column = 2)
+kpPlotRegions(kp, data=positions, col="#FFEECC", border="#FFCCAA", r0=0.3, r1=0.55)
+dev.off()
 
-# Display the plot
-plot(kp)
