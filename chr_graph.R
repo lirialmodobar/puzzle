be1544439a1ab6fcf23ae515328866ee1b29ea63
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-05-18 23:01:51 +0000

    ...

--- chr_graph.R
+++ chr_graph.R
@@ -7,12 +7,12 @@ library(karyoploteR)
 # Read the data file
 dados <- read.table("nat/chr_1_nat_sort_filt_size_gap.txt", header = FALSE, sep=' ')
 positions <- dados[2:4]
+positions[,1] <- paste0("chr", positions[,1])
 colnames(positions) <- c("chr", "start", "end")
 print(positions)
-class(positions)
 #positions <- data.frame(chr=c("chr1", "chr1", "chr1", "chr1"), start=c(30e6, 70e6, 150e6, 180e6), end=c(50e6, 90e6, 170e6, 200e6))
 # Extract the chromosome from your data (assuming all values in the second column are the same)
-chromosome <- paste0("chr", positions[1,1])
+chromosome <- positions[1, 1]
 
 pdf("karyotype_plot.pdf")
 # Create a karyotype object for chromosome drawing with the extracted chromosome
@@ -25,5 +25,10 @@ num_fragments <- nrow(positions)
 height <- 0.1
 
 # Add genomic regions of the specified chromosome to the plot, one above the other
-kpPlotRegions(kp, data = positions, col = "#EEFFCC", border = darker("#EEFFCC"), r0 = 0.3, r1 = 0.55)
+for (i in 1:num_fragments) {
+  r0 <- i * height
+  r1 <- r0 + height
+  kpPlotRegions(kp, data = positions[i, ], col = "#EEFFCC", border = darker("#EEFFCC"), r0 = r0, r1 = r1)
+}
+
 dev.off()
