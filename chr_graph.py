0a0aa16e5c5e894d52249d894781b3a2c3af2d5d
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-05-18 20:21:00 +0000

    tentativa no r

--- chr_graph.py
+++ chr_graph.py
@@ -1,35 +1,23 @@
-import sys
-import os
+# Import the required libraries
+import pyGenomeTracks as pygt
 import pandas as pd
-from pygenomeviz import GenomeViz
 
-# Extract the positions file name, label, and output path from the command-line arguments
-positions_file = "nat/chr_1_nat_sort_filt_size_gap.txt"
-label = "NAT"
-output_path = "nat/"
+# Read the data file
+data = pd.read_table("nat/chr_1_nat_sort_filt_size_gap.txt", header=None)
 
-# Read fragment positions from file using pandas
-fragments_df = pd.read_table(positions_file, header=None)
+# Extract the chromosome from your data (assuming all values in the second column are the same)
+chromosome = data.iloc[0, 1]
 
-# Extract the chromosome number, start, and end columns
-chromosome = fragments_df.iloc[0, 1]
-start_positions = fragments_df.iloc[:, 2]
-end_positions = fragments_df.iloc[:, 3]
+# Create a new track configuration
+config = pygt.TrackConfig()
+config.add_track("axis")
+config.add_track("line", chromosome=chromosome, color="blue", linewidth=1)
 
-# Retrieve cytoband information using pygenome
-genome_viewer = GenomeViz("hg38")
-genome_viewer.add_chromosome(chromosome)
+# Add genomic regions to the track
+for _, row in data.iterrows():
+    start = row[2]
+    end = row[3]
+    config.add_region(chromosome, start, end)
 
-# Plot the fragments
-for start, end in zip(start_positions, end_positions):
-    genome_viewer.add_feature(chromosome, start, end, color="red")
-
-# Set labels and title
-genome_viewer.set_labels(xlabel="Chromosome Position (hg38)")
-genome_viewer.set_title(f"Fragment Positions on {chromosome} - Label: {label}")
-
-# Save the plot
-output_file = os.path.join(output_path, f"fragment_positions_{chromosome}_{label}.png")
-genome_viewer.save(output_file)
-
-print("Chromosome visualization with fragment positions saved successfully!")
+# Plot the genome tracks
+pygt.tracks.plot_tracks([config])
