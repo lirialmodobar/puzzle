4df5ba3b41fd14333542ff7d567b709a63285824
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-05-19 00:30:02 +0000

    ...

--- chr_graph.R
+++ chr_graph.R
@@ -10,8 +10,8 @@ positions <- dados[2:4]
 positions[,1] <- paste0("chr", positions[,1])
 start <- positions[[2]]
 end <- positions[[3]]
-#print(positions)
-positions <- data.frame(chr=c("chr1", "chr1", "chr1", "chr1"), start=c(1, 15, 40, 50), end=c(150, 300, 500, 1000))
+print(positions)
+positions <- data.frame(chr=c("chr1", "chr1", "chr1", "chr1"), start=c(1, 15, 30, 50), end=c(150, 300, 400, 700))
 # Extract the chromosome from your data (assuming all values in the second column are the same)
 chromosome <- positions[1, 1]
 
