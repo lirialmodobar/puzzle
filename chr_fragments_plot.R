ff95a8dd98f554dabf5b77caa3d3fd5cf5e5bfb2
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-06-15 23:02:43 +0000

    changed the colour pallete to one more adequate, 1 colour/frag

--- chr_fragments_plot.R
+++ chr_fragments_plot.R
@@ -7,19 +7,16 @@ library(renv)
 renv::restore()
 # Load the required packages
 library(karyoploteR)
-library(RColorBrewer)
+library(viridis)
 # Get command line arguments
 args <- commandArgs(trailingOnly = TRUE)
-
 # Check if the correct number of arguments is provided
 if (length(args) != 2) {
   stop("Please provide the positions file path and the PDF file name (without .pdf extension) as command line arguments.")
 }
-
 # Extract the arguments
 positions_file <- args[1]
 pdf_file <- paste0(args[2], ".pdf")
-
 # Read the data file
 dados <- read.table(positions_file, header = FALSE, sep = "\t")
 positions <- dados[2:4]
@@ -36,13 +33,12 @@ kpAddLabels(kp, labels = "frag")
 
 # Define the number of fragments
 num_fragments <- nrow(positions)
-
-# Generate a pastel color palette based on the number of fragments using the viridis palette
-colors <- brewer.pal(num_fragments, "Pastel1")
-
+# Generate a rainbow color ramp based on the number of fragments using the viridis color palette with turbo option
+color_ramp <- colorRampPalette(viridis(num_fragments, option = "turbo"))
+# Generate colors using the color ramp
+colors <- color_ramp(num_fragments)
 # Add genomic regions of the specified chromosome to the plot, one above the other, with different colors
 kpPlotRegions(kp, data = positions, col = colors)
-
 # End PDF device
 dev.off()
 
