1bb8a1e2391edcbc36c209b7c3026f185fd9e738
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-10-09 16:34:40 -0300

    bim had duplicates, updated to remove them, as well as filter

--- scripts/filter_bim.sh
+++ scripts/filter_bim.sh
@@ -2,7 +2,8 @@ PLINK2=/usr/bin/plink2
 WD=/mnt/genetica_1/liriel/infos_txt
 COLLAPSE=/mnt/genetica_1/liriel/output_collapse
 [ ! -f "$INFOS_TXT/individuos.txt" ] && ls "$COLLAPSE" | sed -e 's/_A.bed//g; s/_B.bed//g' | sort | uniq > "$INFOS_TXT/individuos.txt"
-IND=$WD/individuos.txt
 awk -v OFS="\t" '{print 0, $1}' $WD/individuos.txt > $WD/input_keep.txt
-$PLINK2 --bfile $WD/BHRC_Probands --keep $WD/input_keep.txt --make-bed --out $WD/BHRC_Probands_filt
+$PLINK2 --bfile $WD/BHRC_Probands --rm-dup force-first --make-bed --out $WD/BHRC_Probands_no_dup
+$PLINK2 --bfile $WD/BHRC_Probands_no_dup --keep $WD/input_keep.txt --make-bed --out $WD/BHRC_Probands_filt
 rm $WD/input_keep.txt
+rm $WD/BHRC_Probands_no_dup*
