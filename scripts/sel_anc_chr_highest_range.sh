148b175fad6560c15af8aa7856f49c9e1b37ffd3
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-10-21 16:40:33 -0300

    generates filt and unfilt files whithout prompting the user

--- scripts/sel_anc_chr_highest_range.sh
+++ scripts/sel_anc_chr_highest_range.sh
@@ -1,5 +1,5 @@
 # Set the working directory
-WD=/home/santoro-lab/liri/teste_dir
+WD=/mnt/genetica_1/liriel/liri_dell/teste_dir
 
 # Define the input files directory
 INPUT_FILES=$WD/output_collapse
@@ -7,13 +7,13 @@ INPUT_FILES=$WD/output_collapse
 # List files in the input directory and save the results to a file
 ls $INPUT_FILES > $WD/collapse_results.txt
 
-# Create the 'infos_txt' directory if it doesn't exist
-if [ ! -d $WD/infos_txt ]; then
-    mkdir $WD/infos_txt
+# Create the 'infos' directory if it doesn't exist
+if [ ! -d $WD/teste/infos ]; then
+    mkdir $WD/teste/infos
 fi
 
-# Set the 'infos_txt' directory path
-INFOS_TXT=$WD/infos_txt
+# Set the 'infos' directory path
+INFOS=$WD/teste/infos
 
 # Process each file listed in 'collapse_results.txt'
 while read collapse; do
@@ -21,56 +21,43 @@ while read collapse; do
     FILE_NAME=$(echo $collapse | head -n 1 | sed "s/.bed//g")
 
     # Extract columns 1-4 from the file and append them to 'all_anc_all_chr.txt'
-    [ ! -f $INFOS_TXT/all_anc_all_chr.txt ] && cut -f1-4 $INPUT_FILES/$collapse | sed "s/^/$FILE_NAME\t/" >> $INFOS_TXT/all_anc_all_chr.txt
+    #[ ! -f $INFOS/all_anc_all_chr.txt ] && 
+    cut -f1-4 $INPUT_FILES/$collapse | sed "s/^/$FILE_NAME\t/" >> $INFOS/all_anc_all_chr.txt
 done < $WD/collapse_results.txt
 
 # Remove the temporary file 'collapse_results.txt'
-rm $WD/collapse_results.txt
-
-read -p "For fragments starting at the same position, select only the largest ones? (y/n): " answer
+#rm $WD/collapse_results.txt
 
 # Define an array of labels
 labels=("NAT" "EUR" "AFR" "UNK")
 
 # Process each label
 for label in "${labels[@]}"; do
-    # Convert label to lowercase
-    label_lower="${label,,}"
+        #Convert label to lowercase
+        label_lower="${label,,}"
 
-    # Create a directory for the label if it doesn't exist
-    if [ ! -d $WD/$label_lower ]; then
-        mkdir $WD/$label_lower
-    fi
-
-ANC_DIR=$WD/$label_lower
-
-# Filter lines with the label (generates file with all the ids, chrs and frags start and ending positions for that label)
-[ ! -f $ANC_DIR/"$label_lower"_all.txt ] && grep -w "$label" $INFOS_TXT/all_anc_all_chr.txt > $ANC_DIR/"$label_lower"_all.txt
-
-    # Process each chromosome (1 to 22)
-    for chr in {1..2}; do
-       	output_prefix="chr_${chr}_${label_lower}_sort"
-        output_sufix="size_gap.txt"
-
-        # Filter lines of the file with all chrs and frags by chromosome, sort them, remove duplicates
-	if [[ $answer == "y" ]]; then
-    	CHRS=$(awk -v chr=$chr '{ if ($2 == chr) {print}}' $ANC_DIR/"$label_lower"_all.txt | sort -k3,3nb -k4,4nbr | awk '!seen[$3]++')
-	output_dir=$ANC_DIR/"chr_info_filt_largest_frag"; [ ! -d "$output_dir" ] && mkdir "$output_dir"
-	output=$output_dir/"$output_prefix"_filt_$output_sufix
-
-        # Filter lines of the file with all chrs and frags by chromosome, sort them
-	else
-    	CHRS=$(awk -v chr=$chr '{ if ($2 == chr) {print}}' $ANC_DIR/"$label_lower"_all.txt | sort -k3,3nb -k4,4nbr)
-	output_dir=$ANC_DIR/"chr_info_unfilt"; [ ! -d "$output_dir" ] && mkdir "$output_dir"
-	output=$output_dir/"$output_prefix"_unfilt_$output_sufix
-	fi
-
-
-        # Calculate frag size and check for gaps, generating one file per chr within the label with all the info related to the frags
-        if [ -n "$CHRS" ]; then
-		echo "$CHRS" | awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' > $output
+        #Create a directory for the label if it doesn't exist
+        if [ ! -d $WD/teste/$label_lower ]; then
+                mkdir $WD/teste/$label_lower
         fi
-    done
+        ANC_DIR=$WD/teste/$label_lower
+        if [ ! -d $WD/teste/$label_lower/chr_info_filt ]; then
+                mkdir $WD/teste/$label_lower/chr_info_filt
+        fi
+        if [ ! -d $WD/teste/$label_lower/chr_info_unfilt ]; then
+                mkdir $WD/teste/$label_lower/chr_info_unfilt
+        fi
+	# Filter lines with the label (generates file with all the ids, chrs and frags start and ending positions for that label)
+	#[ ! -f $ANC_DIR/"$label_lower"_all.txt ] && 
+	grep -w "$label" $INFOS/all_anc_all_chr.txt > $ANC_DIR/"$label_lower"_all.txt
+	# Process each chromosome (1 to 22)
+        for chr in {1..22}; do
+                awk -v chr="$chr" -v OFS="\t" '{ if ($2 == chr) {print}}' "${ANC_DIR}/${label_lower}_all.txt" | sort -k3,3nb -k4,4nbr > $WD/"$chr"_"$label_lower"_temp.txt
+                # Calculate frag size and check for gaps, generating one file per chr within the label with all the info related to the frags
+                if [ -f $WD/"$chr"_"$label_lower"_temp.txt ]; then
+                        awk '!seen[$3]++' $WD/"$chr"_"$label_lower"_temp.txt | awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' > "$ANC_DIR/chr_info_filt/chr_${chr}_${label_lower}_sort_filt_size_gap.txt"
+                        awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' $WD/"$chr"_"$label_lower"_temp.txt > "$ANC_DIR/chr_info_unfilt/chr_${chr}_${label_lower}_sort_unfilt_size_gap.txt"
+                fi
+		rm $WD/"$chr"_"$label_lower"_temp.txt
+        done
 done
-
-# End of the script
