8c6d8bd956ea3145e765cec44a40858be178581c
yurikayel <iury.fontana-terceiro@cea.com.br>
2024-01-10 17:13:57 -0300

    now theres a previous stage generating data for both states together and each state individually

--- scripts/frag_info_per_anc_chr.sh
+++ scripts/frag_info_per_anc_chr.sh
@@ -12,15 +12,31 @@ process_chromosome() {
     local label=$2
     local ANC_DIR=$3
 
-    awk -v chr="$chr" -v OFS="\t" '{ if ($2 == chr) {print}}' "${ANC_DIR}/${label}_all.txt" | sort -k3,3nb -k4,4nbr > "$WD/$chr_${label}_temp.txt"
+    awk -v chr="$chr" -v OFS="\t" '{ if ($2 == chr) {print}}' "${ANC_DIR}/${label}_${state}_all.txt" | sort -k3,3nb -k4,4nbr > "$WD/$chr_${label}_temp.txt"
 
     if [ -s "$WD/$chr_${label}_temp.txt" ]; then
-        awk '!seen[$3]++' "$WD/$chr_${label}_temp.txt" | awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' > "$ANC_DIR/chr_info_filt/chr_${chr}_${label}_sort_filt_size_gap.txt"
+        awk '!seen[$3]++' "$WD/$chr_${label}_temp.txt" | awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5, $6; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5, $6; a=$4}' > "$ANC_DIR/chr_info_filt/chr_${chr}_${label}_${state}_filt.txt"
 
-        awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5; a=$4}' "$WD/$chr_${label}_temp.txt" > "$ANC_DIR/chr_info_unfilt/chr_${chr}_${label}_sort_unfilt_size_gap.txt"
+        awk 'NR==1 {a=$4; printf "%s\t%d\t%d\t%d\t%.2f\tNA\t%s\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, $5, $6; next} {printf "%s\t%d\t%d\t%d\t%.2f\t%d\t%s\t%s\n", $1, $2, $3, $4, ($4-$3)/1000, ($3 <= a) ? 0 : 1, $5, $6; a=$4}' "$WD/$chr_${label}_temp.txt" > "$ANC_DIR/chr_info_unfilt/chr_${chr}_${label}_${state}_unfilt.txt"
     fi
     rm "$WD/$chr_${label}_temp.txt"
 }
+
+# Function to process states and ancestries
+
+process_states_ancs() {
+	local state=$1
+	local label_lower=$2
+
+	if [ "$state" = "rs_sp" ]; then
+		grep -w "$label" "$INFOS/all_anc_all_chr.txt" | awk '{if ($1 ~ /^C1*/) print $0 "\trs"; else print $0 "\tsp"}' > "$ANC_DIR/${label_lower}_${state}_all.txt"
+	elif [ "$state" = "rs" ]; then
+		awk '{if ($6 == "rs") print $0}' $WD/rs_sp/$label_lower/"$label_lower"_rs_sp_all.txt > $ANC_DIR/"$label_lower"_"$state"_all.txt
+	else
+		awk '{if ($6 == "sp") print $0}' $WD/rs_sp/$label_lower/"$label_lower"_rs_sp_all.txt > $ANC_DIR/"$label_lower"_"$state"_all.txt
+	fi
+}
+
 
 # Parse command line arguments
 while [[ "$#" -gt 0 ]]; do
@@ -51,37 +67,47 @@ done < "$WD/collapse_results.txt"
 # Remove the temporary file 'collapse_results.txt'
 rm "$WD/collapse_results.txt"
 
+#Define an array of states
+states=("rs_sp" "rs" "sp")
+
 # Define an array of labels
 labels=("NAT" "EUR" "AFR" "UNK")
 
-# Process each label
-for label in "${labels[@]}"; do
-    label_lower="${label,,}"
-    ANC_DIR="$WD/$label_lower"
+# Process each state and label
+for state in "${states[@]}"; do
+	STATE_DIR=$WD/$state
+	if [ ! -d "$STATE_DIR" ]; then
+        	mkdir "$STATE_DIR"
+    	fi
 
-    # Create a directory for the label if it doesn't exist
-    if [ ! -d "$ANC_DIR" ]; then
-        mkdir "$ANC_DIR"
-    fi
+	for label in "${labels[@]}"; do
+    		label_lower="${label,,}"
+    		ANC_DIR="$STATE_DIR/$label_lower"
 
-    if [ ! -d "$ANC_DIR/chr_info_filt" ]; then
-        mkdir "$ANC_DIR/chr_info_filt"
-    fi
+   	 	# Create a directory for the label if it doesn't exist
+    		if [ ! -d "$ANC_DIR" ]; then
+        		mkdir "$ANC_DIR"
+    		fi
 
-    if [ ! -d "$ANC_DIR/chr_info_unfilt" ]; then
-        mkdir "$ANC_DIR/chr_info_unfilt"
-    fi
+    		if [ ! -d "$ANC_DIR/chr_info_filt" ]; then
+        		mkdir "$ANC_DIR/chr_info_filt"
+    		fi
 
-    # Filter lines with the label
-    grep -w "$label" "$INFOS/all_anc_all_chr.txt" > "$ANC_DIR/${label_lower}_all.txt"
+    		if [ ! -d "$ANC_DIR/chr_info_unfilt" ]; then
+        		mkdir "$ANC_DIR/chr_info_unfilt"
+    		fi
 
-    # Process chromosomes based on the --chr flag
-    if [ -n "$chr_flag" ]; then
-        process_chromosome "$chr_flag" "$label_lower" "$ANC_DIR"
-    else
-        # Process each chromosome from 1 to 22
-        for chr in {1..22}; do
-            process_chromosome "$chr" "$label_lower" "$ANC_DIR"
-        done
-    fi
+    		# Process states and ancs
+		process_states_ancs "$state" "$label_lower"
+
+    		# Process chromosomes based on the --chr flag
+    		if [ -n "$chr_flag" ]; then
+        		process_chromosome "$chr_flag" "$label_lower" "$ANC_DIR"
+    		else
+        	# Process each chromosome from 1 to 22
+        		for chr in {1..22}; do
+            			process_chromosome "$chr" "$label_lower" "$ANC_DIR"
+        		done
+		fi
+	done
 done
