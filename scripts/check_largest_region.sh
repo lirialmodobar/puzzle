bfe048e916752cc12be76d7152679421e1a22759
lirialmodobar <liriel.almodobar@gmail.com>
2024-02-27 17:56:27 -0300

    made it so that i could get this data per state and the largest for both states

--- scripts/check_largest_region.sh
+++ scripts/check_largest_region.sh
@@ -1,10 +1,13 @@
-WD=/home/santoro-lab/liri/teste_dir #set working dir
-label="unk" #change to whatever label you're analysing
-LABEL_DIR=$WD/unk #change to dir with the chr files of the label
-INFOS_TXT=$WD/infos_txt
-for chr in {1..22}; do
-	awk '{print $2,$3,$4,$5}' $LABEL_DIR/chr_info_filt_largest_frag/chr_"${chr}"_"${label}"_sort_filt_size_gap.txt >> $WD/"$label"_chr_sizes.txt
-        sort -k4,4nbr -o $WD/"$label"_chr_sizes.txt $WD/"$label"_chr_sizes.txt
-	awk 'NR==1 {print; largest_frag = $4; next} {if ($4 == largest_frag) {print $0}}' $WD/"$label"_chr_sizes.txt > $INFOS_TXT/largest_"$label"_frag_size.txt
-        rm $WD/"$label"_chr_sizes.txt
+WD=/home/liri/puzzle #set working dir
+INFOS=$WD/infos
+anc="unk" #change this last one to the anc you wanna check
+states=("rs_sp", "rs", "sp")
+for state in "${states[@]}"; do
+	LABEL_DIR=$WD/$state/$anc #change this last one to the anc you wanna check
+	for chr in {1..22}; do
+		awk '{print $2,$3,$4,$5}' $LABEL_DIR/chr_info_filt/chr_"${chr}"_"${anc}"_"${state}"_filt.txt >> $WD/"$label"_chr_sizes.txt
+        	sort -k4,4nbr -o $WD/"$label"_chr_sizes.txt $WD/"$label"_chr_sizes.txt
+		awk 'NR==1 {print; largest_frag = $4; next} {if ($4 == largest_frag) {print $0}}' $WD/"$label"_chr_sizes.txt > $INFOS/largest_"$label"_"$state"_frag.txt
+        	rm $WD/"$label"_chr_sizes.txt
+	done
 done
