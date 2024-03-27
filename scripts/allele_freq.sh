faa0181a5d326fe4775dbb79f303f988256d4dc5
lirialmodobar <liriel.almodobar@gmail.com>
2024-03-27 10:36:01 -0300

    decided that i will not separate the scripts; update notes

--- scripts/allele_freq.sh
+++ scripts/allele_freq.sh
@@ -112,22 +112,13 @@ rm $WD/haps_geno_header.txt
 rm $WD/geno_headless.txt
 
 # ----------------------------------------------------------------------------------
-#WD=/home/santoro/liri/puzzle
-# Check if all three arguments are provided
-#if [ $# -ne 3 ]; then
-#    echo "Usage: $0 <rs> <eur> <chr>"
-#    exit 1
-#fi
 
-# Assigning arguments to variables
-#state=$1
-#anc=$2
-#chr=$3
+#Obtaining allele frequency through processed haps file
 
 INPUT_FILE="$WD/$state/$anc/chr_info_unfilt/chr_${chr}_${anc}_${state}_unfilt.txt"
 INPUT_DIR="$WD/$state/$anc/chr_info_unfilt"
 
-#Create necessary dirs for future steps
+##Create necessary dirs for future steps
 if [ ! -d "$INPUT_DIR/count_info" ]; then
         mkdir "$INPUT_DIR/count_info"
 fi
@@ -137,18 +128,18 @@ if [ ! -d "$INPUT_DIR/seq_info" ]; then
 fi
 
 
-#Functions
+##Functions
 
-##Find haps data in collapse intervals (merge collapse and haps)
+###Find haps data in collapse intervals (merge collapse and haps)
 
 find_vars_within_pos_range() {
 	local pos_file="$1"
 	local var_file="$2"
 	local output_file="$3"
 
-	### Process each line in pos file
+	#### Process each line in pos file
 	while read -r id chrom initial_pos final_pos _ _ _ _; do
-		### Search for matching positions in bim file
+		#### Search for matching positions in bim file
 		vars=$(awk -v OFS="\t" -v id="$id" -v ip="$initial_pos" -v fp="$final_pos" '$1 == id && $4 > ip && $4 < fp { vals = vals  $3 "," $4 "," $5 "\t" } END { print vals }' "$var_file")
 		if [ -n "$vars" ]; then #testing only
 			echo -e "$id\t$chrom\t$initial_pos\t$final_pos\t$vars" >> "$output_file"
@@ -156,35 +147,35 @@ find_vars_within_pos_range() {
 	done < "$pos_file"
 }
 
-##Obtain allele frequencies
+###Obtain allele frequencies
 
-### Count occurrences of var_allele combinations
+#### Count occurrences of var_allele combinations
 
 count_var_allele(){
 	count=$(grep -w "$var_allele" "$cohaps" | wc -l)
 	echo -e "$var_allele\t$chr\t$count\t" | sed 's#,#\t#g' | awk -v OFS="\t" '{print $1, $4, $2, $3, $5}' >> "$WD/count_var_allele.txt"
 }
 
-### Get allele frequencies
+#### Get allele frequencies
 allele_freq(){
 	sum_var=$(grep "$var" "$WD/count_var_allele.txt" | awk '{ sum += $5 } END { print sum }')
 	awk -v OFS="\t" -v var=$var -v sum_var=$sum_var '{if ($1 == var) {print $1, $2, $3, $4, $5, sum_var, ($5/sum_var)*100}}' "$WD/count_var_allele.txt" >> "$INPUT_DIR/count_info/freqs_chr_${chr}_${anc}_${state}.txt"
 }
 
-#Main script
+##Main script
 
 find_vars_within_pos_range $INPUT_FILE "$WD/infos/geno_info_chr_${chr}.txt" "$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
 
 cohaps="$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
 
-## Getting variant_alleles combinations to search
+### Getting variant_alleles combinations to search
 awk '{for(i=5;i<=NF;i++) print $i}' "$cohaps"  | sort -b | uniq > "$WD/varal_info_entrada.txt"
 
 while read var_allele; do
         count_var_allele "$var_allele"
 done < $WD/varal_info_entrada.txt
 
-##Getting variants to search
+###Getting variants to search
 cut -f 1 "$WD/count_var_allele.txt" | sort -b | uniq  > $WD/temp_input.txt
 
 while read var; do
