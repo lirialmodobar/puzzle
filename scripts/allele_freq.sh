da679e5b5322812b2a7c0b959857b1ea2eca8e57
lirialmodobar <liriel.almodobar@gmail.com>
2024-03-29 20:24:12 -0300

    had to subset haps

--- scripts/allele_freq.sh
+++ scripts/allele_freq.sh
@@ -16,12 +16,30 @@ chr=$3
 
 ##Process .haps file
 
+### Subset haps for my sample
+
+tail +3 INPD_hg38_1.sample | grep -n C | sed s#:0##g | sed 's#_[^ ]*##g' | awk '{ print $1, $2}' | sed "p" |awk 'NR%2{suffix="_A"} !(NR%2){suffix="_B"} {print $0 suffix}' | awk '{if (NR % 2 == 0) $1 = ($1 * 2) + 5 ; else $1 = ($1 * 2) - 1 + 5} 1' > $WD/haps_cols.txt
+awk '{print $1}' $WD/haps_cols.txt > $WD/haps_indexes
+
+#### Declare an empty array
+indexes=()
+
+# Read the file line by line and populate the array
+while IFS= read -r line; do
+    indexes+=("$line")
+done < $WD/haps_indexes
+
+awk -v OFS="\t"  -v indexes="${indexes[*]}" '{split(indexes, arr, " "); for (i in arr) printf "%s ", $arr[i]; print ""}' $haps_file > "$WD/haps_bhrc_children/bhrc_hg38_children_chr_${chr}.haps" 
+
+rm $WD/haps_indexes
+
 ### Generate .haps with header
 
 haps_header() {
-	tail +3 $sample_file | awk '{ print $2}' | sed "p" |awk 'NR%2{suffix="_A"} !(NR%2){suffix="_B"} {print $0 suffix}' | tr '\n' '\t' > $WD/geno_columns.txt
+	awk '{print $2}' $WD/haps_cols.txt | tr '\n' '\t' > $WD/geno_columns.txt
 	paste $WD/infos/5_columns.txt $WD/geno_columns.txt > $WD/header.txt  #5_columns should go to info
 	rm $WD/geno_columns.txt
+	rm $WD/haps_cols.txt
 }
 
 
@@ -40,7 +58,7 @@ get_allele() {
 			}
 		}
 		print modified_line
-	}' $haps_file > $WD/alleles.txt
+	}' $subset_haps_file > $WD/alleles.txt
 }
 
 join_header_allele(){
@@ -51,7 +69,7 @@ join_header_allele(){
 
 get_header_allele(){
 	local sample_file=$1
-	local haps_file=$2
+	local subset_haps_file=$2
 	local header_allele_file=$3
 	haps_header
 	get_allele
@@ -100,7 +118,25 @@ id_as_row(){
 
 #Main script
 
-get_header_allele "$HAPS_SAMPLE_DIR/127_BHRC_altura_chr${chr}.sample" "$HAPS_SAMPLE_DIR/127_BHRC_altura_chr${chr}.haps" $WD/haps_geno_header.txt
+# Subset haps for my sample
+
+tail +3 INPD_hg38_1.sample | grep -n C | sed s#:0##g | sed 's#_[^ ]*##g' | awk '{ print $1, $2}' | sed "p" |awk 'NR%2{suffix="_A"} !(NR%2){suffix="_B"} {print $0 suffix}' | awk '{if (NR % 2 == 0) $1 = ($1 * 2) + 5 ; else $1 = ($1 * 2) - 1 + 5} 1' > $WD/haps_cols.txt
+awk '{print $1}' $WD/haps_cols.txt > $WD/haps_indexes
+
+## Declare an empty array
+indexes=()
+
+### Read the file line by line and populate the array
+
+while IFS= read -r line; do
+    indexes+=("$line")
+done < $WD/haps_indexes
+
+awk -v OFS="\t"  -v indexes="${indexes[*]}" '{split(indexes, arr, " "); for (i in arr) printf "%s ", $arr[i]; print ""}' "$WD/bhrc_haps_sample/127_BHRC_altura_chr${chr}.haps > "$WD/haps_bhrc_children/bhrc_hg38_children_chr_${chr}.haps"
+
+rm $WD/haps_indexes
+
+get_header_allele "$HAPS_SAMPLE_DIR/127_BHRC_altura_chr${chr}.sample" "$WD/haps_bhrc_children/bhrc_hg38_children_chr_${chr}.haps" $WD/haps_geno_header.txt
 
 tail +2 $WD/haps_geno_header.txt > $WD/geno_headless.txt
 
@@ -108,81 +144,81 @@ while read -r row; do
 	id_as_row $WD/haps_geno_header.txt
 done < $WD/geno_headless.txt
 
-rm $WD/haps_geno_header.txt
+#rm $WD/haps_geno_header.txt
 rm $WD/geno_headless.txt
 
 # ----------------------------------------------------------------------------------
 
 #Obtaining allele frequency through processed haps file
 
-INPUT_FILE="$WD/$state/$anc/chr_info_unfilt/chr_${chr}_${anc}_${state}_unfilt.txt"
-INPUT_DIR="$WD/$state/$anc/chr_info_unfilt"
+#INPUT_FILE="$WD/$state/$anc/chr_info_unfilt/chr_${chr}_${anc}_${state}_unfilt.txt"
+#INPUT_DIR="$WD/$state/$anc/chr_info_unfilt"
 
 ##Create necessary dirs for future steps
-if [ ! -d "$INPUT_DIR/count_info" ]; then
-        mkdir "$INPUT_DIR/count_info"
-fi
+#if [ ! -d "$INPUT_DIR/count_info" ]; then
+ #       mkdir "$INPUT_DIR/count_info"
+#fi
 
-if [ ! -d "$INPUT_DIR/seq_info" ]; then
-        mkdir "$INPUT_DIR/seq_info"
-fi
+#if [ ! -d "$INPUT_DIR/seq_info" ]; then
+ #       mkdir "$INPUT_DIR/seq_info"
+#fi
 
 
 ##Functions
 
 ###Find haps data in collapse intervals (merge collapse and haps)
 
-find_vars_within_pos_range() {
-	local pos_file="$1"
-	local var_file="$2"
-	local output_file="$3"
+#find_vars_within_pos_range() {
+#	local pos_file="$1"
+#	local var_file="$2"
+#	local output_file="$3"
 
 	#### Process each line in pos file
-	while read -r id chrom initial_pos final_pos _ _ _ _; do
+#	while read -r id chrom initial_pos final_pos _ _ _ _; do
 		#### Search for matching positions in bim file
-		vars=$(awk -v OFS="\t" -v id="$id" -v ip="$initial_pos" -v fp="$final_pos" '$1 == id && $4 > ip && $4 < fp { vals = vals  $3 "," $4 "," $5 "\t" } END { print vals }' "$var_file")
-		if [ -n "$vars" ]; then #testing only
-			echo -e "$id\t$chrom\t$initial_pos\t$final_pos\t$vars" >> "$output_file"
-		fi
-	done < "$pos_file"
-}
+#		vars=$(awk -v OFS="\t" -v id="$id" -v ip="$initial_pos" -v fp="$final_pos" '$1 == id && $4 > ip && $4 < fp { vals = vals  $3 "," $4 "," $5 "\t" } END { print vals }' "$var_file")
+#		if [ -n "$vars" ]; then #testing only
+#			echo -e "$id\t$chrom\t$initial_pos\t$final_pos\t$vars" >> "$output_file"
+#		fi
+#	done < "$pos_file"
+#}
 
 ###Obtain allele frequencies
 
 #### Count occurrences of var_allele combinations
 
-count_var_allele(){
-	count=$(grep -w "$var_allele" "$cohaps" | wc -l)
-	echo -e "$var_allele\t$chr\t$count\t" | sed 's#,#\t#g' | awk -v OFS="\t" '{print $1, $4, $2, $3, $5}' >> "$WD/count_var_allele.txt"
-}
+#count_var_allele(){
+#	count=$(grep -w "$var_allele" "$cohaps" | wc -l)
+#	echo -e "$var_allele\t$chr\t$count\t" | sed 's#,#\t#g' | awk -v OFS="\t" '{print $1, $4, $2, $3, $5}' >> "$WD/count_var_allele.txt"
+#}
 
 #### Get allele frequencies
-allele_freq(){
-	sum_var=$(grep "$var" "$WD/count_var_allele.txt" | awk '{ sum += $5 } END { print sum }')
-	awk -v OFS="\t" -v var=$var -v sum_var=$sum_var '{if ($1 == var) {print $1, $2, $3, $4, $5, sum_var, ($5/sum_var)*100}}' "$WD/count_var_allele.txt" >> "$INPUT_DIR/count_info/freqs_chr_${chr}_${anc}_${state}.txt"
-}
+#allele_freq(){
+#	sum_var=$(grep "$var" "$WD/count_var_allele.txt" | awk '{ sum += $5 } END { print sum }')
+#	awk -v OFS="\t" -v var=$var -v sum_var=$sum_var '{if ($1 == var) {print $1, $2, $3, $4, $5, sum_var, ($5/sum_var)*100}}' "$WD/count_var_allele.txt" >> "$INPUT_DIR/count_info/freqs_chr_${chr}_${anc}_${state}.txt"
+#}
 
 ##Main script
 
-find_vars_within_pos_range $INPUT_FILE "$WD/infos/geno_info_chr_${chr}.txt" "$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
+#find_vars_within_pos_range $INPUT_FILE "$WD/infos/geno_info_chr_${chr}.txt" "$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
 
-cohaps="$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
+#cohaps="$INPUT_DIR/seq_info/cohaps_chr_${chr}_${anc}_${state}.txt"
 
 ### Getting variant_alleles combinations to search
-awk '{for(i=5;i<=NF;i++) print $i}' "$cohaps"  | sort -b | uniq > "$WD/varal_info_entrada.txt"
+#awk '{for(i=5;i<=NF;i++) print $i}' "$cohaps"  | sort -b | uniq > "$WD/varal_info_entrada.txt"
 
-while read var_allele; do
-        count_var_allele "$var_allele"
-done < $WD/varal_info_entrada.txt
+#while read var_allele; do
+ #       count_var_allele "$var_allele"
+#done < $WD/varal_info_entrada.txt
 
 ###Getting variants to search
-cut -f 1 "$WD/count_var_allele.txt" | sort -b | uniq  > $WD/temp_input.txt
+#cut -f 1 "$WD/count_var_allele.txt" | sort -b | uniq  > $WD/temp_input.txt
 
-while read var; do
-	allele_freq $var
-done < $WD/temp_input.txt
+#while read var; do
+#	allele_freq $var
+#done < $WD/temp_input.txt
 
-rm "$WD/infos/geno_info_chr_${chr}.txt"
-rm $WD/varal_info_entrada.txt
-rm $WD/count_var_allele.txt
-rm $WD/temp_input
+#rm "$WD/infos/geno_info_chr_${chr}.txt"
+#rm $WD/varal_info_entrada.txt
+#rm $WD/count_var_allele.txt
+#rm $WD/temp_input
