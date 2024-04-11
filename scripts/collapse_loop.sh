27cf7c35a98d90a5f0c41fae69f0df907f455cd2
lirialmodobar <liriel.almodobar@gmail.com>
2024-04-11 17:54:51 -0300

    irrelevant changes

--- scripts/collapse_loop.sh
+++ scripts/collapse_loop.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-INPUT_DIR=/mnt/genetica_1/Rafaella/bhrc_lai
-grep C $INPUT_DIR/*.sample > /mnt/genetica_1/liriel/individuos.txt 
+WD=/scratch/unifesp/pgt/liriel.almodobar/puzzle
+INPUT_DIR=$WD/bhrc_lai
+#grep C $INPUT_DIR/*.sample > $WD/infos/individuos.txt 
 while read p; do
-        python3 /mnt/genetica_1/Rafaella/rafathon/ancestry_pipeline-master/collapse_ancestry.py \
-        --rfmix $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr1.2.Viterbi.txt \
-        --snp_locations $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr1.snp_locations \
-        --fbk $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr1.2.ForwardBackward.txt.gz \
+        python3 $WD/scripts/collapse_ancestry.py \
+        --rfmix $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr2.2.Viterbi.txt \
+        --snp_locations $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr2.snp_locations \
+        --fbk $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr2.2.ForwardBackward.txt.gz \
         --fbk_threshold 0.9 \
         --ind "$p"  \
         --ind_info $INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI.sample \
         --pop_labels NAT,EUR,AFR \
-        --out /mnt/genetica_1/liriel/output_collapse/"$p"
-done < /mnt/genetica_1/liriel/individuos.txt
+        --out $WD/output_collapse/chr_2/"$p"
+done < $WD/infos/individuos.txt
