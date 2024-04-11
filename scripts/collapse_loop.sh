000583da47bcb7a46fbb0f17320cf3e0048eda14
lirialmodobar <liriel.almodobar@gmail.com>
2024-04-11 19:02:40 -0300

    parallel

--- scripts/collapse_loop.sh
+++ scripts/collapse_loop.sh
@@ -6,8 +6,6 @@ chr=$1
 if [ ! -d "$WD/output_collapse/chr_${chr}" ]; then
        mkdir "$WD/chr_${chr}"
 fi
-INPUT_DIR=/mnt/genetica_1/Rafaella/bhrc_lai
-grep C $INPUT_DIR/*.sample > /mnt/genetica_1/liriel/individuos.txt 
 while read p; do
         python3 /mnt/genetica_1/Rafaella/rafathon/ancestry_pipeline-master/collapse_ancestry.py \
         --rfmix "$INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI_chr${chr}.2.Viterbi.txt" \
@@ -17,5 +15,6 @@ while read p; do
         --ind "$p"  \
         --ind_info "$INPUT_DIR/BHRC_JOIN_RefHGDPNAT_1kgIBS_1kgYRI.sample" \
         --pop_labels NAT,EUR,AFR \
-	--out "/mnt/genetica_1/liriel/output_collapse/chr_${chr}/$p"
+	--chr $chr
+	--out "$WD/output_collapse/chr_${chr}/$p"
 done < $WD/infos/individuos.txt
