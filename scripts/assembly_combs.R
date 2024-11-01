fd7a76f48ba81d79de2f067b2876b24c61d2867e
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2024-11-01 11:11:32 -0300

    Update assembly_combs.R

--- scripts/assembly_combs.R
+++ scripts/assembly_combs.R
@@ -325,16 +325,13 @@ if(length(args) < 3){
 chr <- args[1]
 state <- args[2]
 anc <- args[3]
-
-base_dir <- "/mnt/genetica_1/liriel"
+base_dir <- "/home/yuri/liri"
 
 
 print(paste("Parameters - chr:", chr, ", state:", state, ", anc:", anc))
 print(paste("Base directory:", base_dir))
 
-
-collapse_dir <- file.path(base_dir, state, anc, "chr_info_unfilt")
-
+collapse_dir <- file.path(base_dir, "puzzle_sdumont", state, anc, "chr_info_unfilt")
 collapse_file <- paste0("chr_", chr, "_", anc, "_", state, "_unfilt.txt")
 collapse_info_path <- file.path(collapse_dir, collapse_file)
 
