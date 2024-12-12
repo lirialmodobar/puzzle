90b84aa91b4be89280a32ad0f102b41cb6fe1eb7
lirialmodobar <liriel.almodobar@gmail.com>
2024-12-12 20:36:21 -0300

    back to stable version

--- scripts/chr_fragments_plot.R
+++ scripts/chr_fragments_plot.R
@@ -1,95 +1,50 @@
+# Load project environment
+if (!requireNamespace("renv", quietly = TRUE)) {
+  # Install renv and its dependencies
+  install.packages("renv", dependencies = TRUE)
+}
+library(renv)
+renv::restore()
+
 # Load the required packages
 library(karyoploteR)
-library(RColorBrewer)
-# Get command line arguments
-args <- commandArgs(trailingOnly = TRUE)
 
 # Set the working directory
-wd <- "/home/yuri/liri/puzzle"  
+wd <- "~/liri/teste_dir"  
 
 # Define the labels and corresponding colors
-states <- c("rs_sp", "rs", "sp")
 labels <- c("nat", "eur", "afr", "unk")
 colors <- c("#6caf5e", "#7297b4", "#c05040", "#919191")
 
-#Create plot
-create_fragments_plot <- function(label, label_dir, state, color, chr, n_rows) {
-	graph_dir <- "frags_plots"
-	if (!file.exists(file.path(label_dir, graph_dir))) {
-		dir.create(file.path(label_dir, graph_dir))
-	}
-	positions_file_prefix <- paste0("chr_", chr, "_", label, state, "_filt")
-	positions_file_path <- file.path(label_dir, paste0(positions_file_prefix, ".txt")
-	# Read the data file
-	dados <- read.table(positions_file_path, header = FALSE, sep = "\t")
-	positions <- dados[2:4]
-	positions[, 1] <- paste0("chr", positions[, 1])
-	set.seed(123)
-        random_indices <- sample(1:n_rows, n_rows)
-        positions <- positions[random_indices, ]
-	# Extract the chromosome from your data (assuming all values in the second column are the same)
-	chromosome <- positions[1, 1]
-	# Start PDF device
-	pdf_file <- file.path(label_dir, graph_dir, paste0(n_rows,positions_file_prefix,"_frags_plot.pdf"))
-	pdf(pdf_file)
-	# Create a karyotype object for chromosome drawing with the extracted chromosome
-	kp <- plotKaryotype(genome = "hg38", chromosomes = chromosome)
-	kpAddLabels(kp, labels = paste("frag", label, state))
-	# Add fragments to the plot
-	kpPlotRegions(kp, data = positions[1:n_rows, ], col=color, border=darker(color), avoid.overlapping=TRUE)
-	# End PDF device
-	dev.off()
-}
-
-option_list <- list(
-	make_option(c("--rows_per_anc"), type = "file", default = NULL,
-              help = "Tab-delimited file with rows and labels (no header)."),
-	make_option(c("--run_as_test"), type = "logical", default = FALSE,
-              help = "Flag to run the test.")
-	make_option(c("--chrom"), type = "logical", default = NULL,
-	      help = "Specify the chromosome to use. If not specified, all chromosomes will be used.")
-)
-
 # Loop through the labels and chromosomes
-for (i in 1:length(states)) {
-	state <- states[i]
-	for (j in 1:lenght(labels)) {
-		label <- labels[j]
-		color <- colors[j]
-		label_dir <- file.path(wd, state, label, "chr_info_filt")
-		if (opt$run_as_test) {
-			files <- list.files(label_dir, full.names = TRUE)
-			max_rows <- 0
-			for (file in files) {
-				# Use the system command to count the number of lines in the file
-				line_count <- as.numeric(system(paste("wc -l", shQuote(file)), intern = TRUE))
-				# Extract the number of rows from the command output
-				num_rows <- line_count[1]
-				# Update max_rows if necessary
-				if (num_rows > max_rows) {
-					max_rows <- num_rows
-				}
-			}
-		rows <- seq(from = 100, to = max_rows, by = 100)
-		} 
-		if (is.null(opt$chromosome)) {
-			chromosomes <- 1:22  # Use all chromosomes
-		} else {
-			chromosomes <- opt$chromosome
-		}
-		# Create vector with the numbers of rows to be tested for that label
-		for (chr in chromosomes) {
-			if (opt$run_as_test) {
-				for (n_rows in rows) {
-					create_fragments_plot(label, label_dir, state,  color, chr, n_rows)
-				}
-			} else {
-				# Ask the user for the value of n_rows
-				n_rows <- rows_data$V1[rows_data$V2 == label][1]
-				create_fragments_plot(label, label_dir, state, color, chr, n_rows)
-			}
-		}
-	}
+for (i in 1:length(labels)) {
+  label <- labels[i]
+  color <- colors[i]
+  for (chr in 1:22) {
+    label_dir <- file.path(wd, label, "chr_info_filt_largest_frag")
+    graph_dir <- "frags_plots"
+    if (!file.exists(file.path(label_dir, graph_dir))) {
+    dir.create(file.path(label_dir, graph_dir))
+    }
+    file_name_prefix <- paste0("chr_", chr, "_", label)
+    positions_file <- file.path(label_dir, paste0(file_name_prefix, "_sort_filt_size_gap.txt"))
+    pdf_file <- file.path(label_dir, graph_dir, paste0(file_name_prefix, "_plot_fragments.pdf"))
+    # Read the data file
+    dados <- read.table(positions_file, header = FALSE, sep = "\t")
+    positions <- dados[2:4]
+    positions[, 1] <- paste0("chr", positions[, 1])
+    # Extract the chromosome from your data (assuming all values in the second column are the same)
+    chromosome <- positions[1, 1]
+    # Start PDF device
+    pdf(pdf_file)
+    # Create a karyotype object for chromosome drawing with the extracted chromosome
+    kp <- plotKaryotype(genome = "hg38", chromosomes = chromosome)
+    kpAddLabels(kp, labels = paste("frag", label))
+    # Add fragments to the plot
+    kpPlotRegions(kp, data = positions, col=color, border=darker(color), avoid.overlapping=TRUE)
+    # End PDF device
+    dev.off()     
+  }
 }
 # Save current environment
 renv::snapshot()
