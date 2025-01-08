9d7ce8882826a57cc23b02c32631917b94fda347
lirialmodobar <liriel.almodobar@gmail.com>
2025-01-08 13:46:36 -0300

    enrichment

--- scripts/comp_allele_freq_rs_sp.R
+++ scripts/comp_allele_freq_rs_sp.R
@@ -3,87 +3,239 @@ library(rtracklayer)
 library(fuzzyjoin)
 library(tidyr)
 
-##Data import
-rs_dir <- "/home/yuri/liri/puzzle/rs/nat/chr_info_unfilt/count_info"
-sp_dir <- "/home/yuri/liri/puzzle/sp/nat/chr_info_unfilt/count_info"
-rs_file <- file.path(rs_dir, "freqs_chr_5_nat_rs.txt")
-sp_file <- file.path(sp_dir, "freqs_chr_5_nat_sp.txt")
-rs <- read.table(rs_file, sep = "\t")
-colnames(rs) <- c("SNP", "CHR", "BP", "allele", "count_rs", "total_rs", "freq_rs") #not sure if i need that
-sp <- read.table(sp_file, sep = "\t")
+organize_enrichment_results <- function(enrichResult)  {
+  #Filters for significant results adjusted by multiple comparisions
+  #Orders by gene ratio and then p.adjust
+  
+  ###Filter for significant results 
+  
+  results_df <- enrichResult@result
+  results_significant <-results_df[results_df$p.adjust < 0.05,]
+  
+  ###Order by p.adjust and gene ratio
+  if (!is.null(results_significant) && nrow(results_significant) != 0) {
+    #### Split the GeneRatio values by the '/' character
+    gene_ratio_parts <- strsplit(results_significant$GeneRatio, "/")
+    
+    #### Perform the division and store the results
+    results_significant$GeneRatio <- sapply(gene_ratio_parts, function(x) as.numeric(x[1]) / as.numeric(x[2]))
+    
+    #### Now order by p.adjust (ascending) and then GeneRatio (descending) 
+    results_significant <- results_significant[order(results_significant$p.adjust, -results_significant$GeneRatio), ]
+    return(results_significant)
+  }
+}
+
+# Directories
+rs_dir <- "/home/yuri/liri/puzzle_sdumont/rs/nat/chr_info_unfilt/count_info"
+sp_dir <- "/home/yuri/liri/puzzle_sdumont/sp/nat/chr_info_unfilt/count_info"
+
+# Initialize empty data frames to store results for all chromosomes
+vep_combined <- data.frame()
+diff_vars_combined <- data.frame()
+rsvars_only_combined <- data.frame()
+spvars_only_combined <- data.frame()
+
+# Loop through chromosomes 1 to 22
+for (chr in 1:22) {
+  # File paths for current chromosome
+  rs_file <- file.path(rs_dir, paste0("freqs_chr_", chr, "_nat_rs.txt"))
+  sp_file <- file.path(sp_dir, paste0("freqs_chr_", chr, "_nat_sp.txt"))
+  
+  # Read and filter data
+  rs <- read.table(rs_file, sep = "\t", header = FALSE)
+  sp <- read.table(sp_file, sep = "\t", header = FALSE)
+  
+  colnames(rs) <- c("SNP", "CHR", "BP", "allele", "count_rs", "total_rs", "freq_rs")
+  colnames(sp) <- c("SNP", "CHR", "BP", "allele", "count_sp", "total_sp", "freq_sp")
+  
+  rs <- subset(rs, total_rs > 10)
+  sp <- subset(sp, total_sp > 10)
+  
+  # Extract variables of interest
+  rsvars <- rs[, c("SNP", "allele", "freq_rs")]
+  spvars <- sp[, c("SNP", "allele", "freq_sp")]
+  
+  colnames(rsvars) <- c("var", "allele", "freq")
+  colnames(spvars) <- c("var", "allele", "freq")
+  
+  # Common variants and frequency difference
+  rssp_comm <- inner_join(rsvars, spvars, by = c("var", "allele"), suffix = c(".rsvars", ".spvars"))
+  rssp_comm <- rssp_comm %>%
+    mutate(allele_freq_diff_rssp = round(abs(freq.rsvars - freq.spvars), digits = 2))
+  
+  diff_vars <- subset(rssp_comm, abs(allele_freq_diff_rssp - mean(allele_freq_diff_rssp)) > (3 * sd(allele_freq_diff_rssp)))
+  
+  # Append diff_vars to combined data frame
+  diff_vars_combined <- bind_rows(diff_vars_combined, diff_vars)
+  
+  # Get rsvars_only and spvars_only
+  rsvars_only <- anti_join(rsvars, spvars, by = c("var", "allele"))
+  spvars_only <- anti_join(spvars, rsvars, by = c("var", "allele"))
+  
+  # Append to combined data frames
+  rsvars_only_combined <- bind_rows(rsvars_only_combined, rsvars_only)
+  spvars_only_combined <- bind_rows(spvars_only_combined, spvars_only)
+  
+  # VEP preparation
+  vep <- inner_join(rs, diff_vars, by = c("SNP" = "var", "allele"))
+  vep <- vep %>%
+    mutate(SNP = paste(CHR, BP, sep = ":")) %>%
+    select(SNP, CHR, BP, allele, freq_rs)
+  
+  subset_not_50 <- subset(vep, freq_rs != 50)
+  subset_50_even <- subset(vep, freq_rs == 50 & (1:nrow(vep)) %% 2 == 0)
+  subset_50_odd <- subset(vep, freq_rs == 50 & (1:nrow(vep)) %% 2 == 1)
+  
+  vep_a1 <- subset_not_50 %>%
+    group_by(SNP, CHR, BP) %>%
+    slice_max(order_by = freq_rs) %>%
+    ungroup() %>%
+    bind_rows(subset_50_even)
+  
+  vep_a2 <- subset_not_50 %>%
+    group_by(SNP, CHR, BP) %>%
+    slice_min(order_by = freq_rs) %>%
+    ungroup() %>%
+    bind_rows(subset_50_odd)
+  
+  colnames(vep_a1) <- c("SNP", "CHR", "BP", "A2", "freq_A2")
+  colnames(vep_a2) <- c("SNP", "CHR", "BP", "A1", "freq_A1")
+  
+  vep_chr <- inner_join(vep_a1, vep_a2, by = c("SNP", "CHR", "BP")) %>%
+    select(SNP, CHR, BP, A1, A2) %>%
+    mutate(BP2 = BP, strand = 1, allele = paste(A2, A1, sep = "/")) %>%
+    select(SNP, CHR, BP, BP2, strand, allele) %>%
+    arrange(CHR, BP)
+  
+  # Append to combined VEP data frame
+  vep_combined <- bind_rows(vep_combined, vep_chr)
+}
+
+# Write combined data frames to files
+write.csv(diff_vars_combined, "diff_vars_combined.csv", row.names = FALSE, quote = FALSE)
+write.csv(rsvars_only_combined, "rsvars_only_combined.csv", row.names = FALSE, quote = FALSE)
+write.csv(spvars_only_combined, "spvars_only_combined.csv", row.names = FALSE, quote = FALSE)
+
+# Write final combined VEP file
+write.table(vep_combined, "vep_combined.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
 
 
-##Data exploration
-rsvars <- rs[, c(1,4,7)]
-spvars <- sp[, c(1,4,7)]
-colnames(rsvars) <- c("var", "allele", "freq")
-colnames(spvars) <- c("var", "allele", "freq")
-rssp_comm <- inner_join(rsvars, spvars, by = c("var", "allele"), suffix = c(".rsvars", ".spvars"))
-rssp_comm <- rssp_comm %>% mutate(allele_freq_diff_rssp = round(abs(.[3] - .[4]), digits = 2))
-rssp_0 <- subset(rssp_comm, rssp_comm[[5]] == 0)
-rssp_maior_0 <- subset(rssp_comm, rssp_comm[[5]] > 0)
-rssp_maior_1 <- subset(rssp_comm, rssp_comm[[5]] >= 1)
-smallest_diff <- min(rssp_maior_0[[5]])
-smallest_non_0_diff <- min(rssp_maior_1[[5]])
-rsvars_only <- anti_join(rsvars, spvars, by = c("var", "allele"))
-spvars_only <- anti_join (spvars, rsvars, by = c("var", "allele"))
-write.csv(rsvars_only, "rs_vars_only_5.csv", quote = FALSE, row.names = FALSE)
-write.csv(spvars_only, "sp_vars_only_5.csv", quote = FALSE, row.names = FALSE)
+##Enrichment analysis
 
-## Diff analysis
-sd_3_rssp_diff <- 3*round(sd(rssp_comm$allele_freq_diff_rssp$freq.rsvars), digits = 2)
-outliersvars_high_diff <- subset(rssp_comm, rssp_comm[[5]] > sd_3_rssp_diff)
-outliersvars_high_diff_5 <- as.data.frame(outliersvars_high_diff$allele_freq_diff_rssp$freq.rsvars)
-outliersvars_high_diff <- cbind(outliersvars_high_diff, outliersvars_high_diff_5)
-outliersvars_high_diff <- outliersvars_high_diff[, c(1,2,3,4,6)]
-write.csv(outliersvars_high_diff, "vars_high_diff_chr5.csv", quote = FALSE, row.names = FALSE)
+saving_dir <- "/home/yuri/liri/puzzle/"
 
-smallest_high_diff <- min(outliersvars_high_diff[[5]])
+### Retrieve gene information (chromosome, start, end positions, gene names)
 
-## VEP input
+ensembl = useMart("ensembl", dataset = "hsapiens_gene_ensembl", host = "https://asia.ensembl.org")
 
-vep <- inner_join(rs, outliersvars_high_diff, by =  c("SNP" = "var", "allele"))
-vep <- vep %>%
-  mutate(SNP = paste(CHR, BP, sep = ":"))
-vep <- fuma[, c(1,2,3,4,7)]
+gene_gr <- getBM(
+  attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", "start_position", "end_position"),
+  mart = ensembl
+)
 
-subset_not_50 <- subset(vep, vep[[5]] != 50)
-subset_50_even <- subset(vep, vep[[5]] == 50 & (1:nrow(vep)) %% 2 == 0)
-subset_50_odd <- subset(vep, vep[[5]] == 50 & (1:nrow(vep)) %% 2 == 1)
+## Convert gene information into GenomicRanges object
+gene_gr <- GRanges(
+  seqnames = gene_gr$chromosome_name,
+  ranges = IRanges(start = gene_gr$start_position, end = gene_gr$end_position),
+  gene_id = gene_gr$ensembl_gene_id,
+  gene_name = gene_gr$external_gene_name
+)
+## Create GenomicRanges object for the SNVs
+    snv_gr <- GRanges(
+      seqnames = vep_combined$CHR, 
+      ranges = IRanges(
+        start = vep_combined$BP,  # SNV positions
+        end = vep_combined$BP  # Same position for the end
+      )
+    )
 
-vep_a1 <-subset_not_50 %>%
-  group_by(SNP, CHR, BP) %>%
-  slice_max(order_by = freq_rs) %>%
-  ungroup()
-vep_a1 <- rbind(vep_a1, subset_50_even)
-vep_a2 <-subset_not_50 %>%
-  group_by(SNP, CHR, BP) %>%
-  slice_min(order_by = freq_rs) %>%
-  ungroup()
-vep_a2 <- rbind(vep_a2, subset_50_odd)
-colnames(vep_a1) <- c("SNP", "CHR", "BP", "A2", "freq_A2")
-colnames(vep_a2) <- c("SNP", "CHR", "BP", "A1", "freq_A1")
-vep <- inner_join(vep_a1, vep_a2, by = c("SNP", "CHR", "BP"))
-vep <- vep[,c(1,2,3,6,4)]
-
-vep <- vep[-1]
-vep <- vep %>% 
-  mutate(BP2 = BP, strand = 1, allele = paste(A2, A1, sep = "/")) %>%
- select(1, 2, 5, 7, 6) %>%
- arrange(CHR, BP)
-
-write.table(vep, "vep_5.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
-
-##All chromosomes vep input
-
-vep_1 <- read.table("/home/yuri/liri/puzzle/vep_input/vep.tsv")
-vep_2 <- read.table("/home/yuri/liri/puzzle/vep_input/vep_2.tsv")
-vep_3 <- read.table("/home/yuri/liri/puzzle/vep_input/vep_3.tsv")
-vep_4 <- read.table("/home/yuri/liri/puzzle/vep_input/vep_4.tsv")
-vep_5 <- read.table("/home/yuri/liri/puzzle/vep_input/vep_5.tsv")
-
-vep_1_5 <- rbind(vep_1, vep_2, vep_3, vep_4, vep_5)
-
-vep_1_5 <- vep_1_5[order(vep_1_5$V1, vep_1_5$V2), ]
-
-write.table(vep_1_5, "vep_1_5.tsv", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+  
+  ## Find Genes Overlapping SNVs
+  overlap_genes <- findOverlaps(snv_gr, gene_gr)
+  overlap_gene_indices <- subjectHits(overlap_genes)
+  overlap_gene_names <- gene_gr$gene_name[overlap_gene_indices]
+  overlap_gene_ensembl_id <- gene_gr$gene_id[overlap_gene_indices]
+  overlap_gene_data <- DataFrame(
+    gene_name = overlap_gene_names,
+    ensembl_id = overlap_gene_ensembl_id
+  )
+  gene_info <- ifelse(is.na(overlap_gene_names) | overlap_gene_names == "", 
+                      overlap_gene_ensembl_id, 
+                      overlap_gene_names)
+  
+  if(!is.null(gene_info) && length(gene_info) != 0) {
+    ## Get the list of gene names from the overlaps
+    genes_list_go <- unique(gene_info)  # Remove duplicates
+    
+    ## Convert to ENTREZ ids (necessary for KEGG enrichment)
+    
+    entrez_mapping <- AnnotationDbi::select(org.Hs.eg.db, keys = overlap_gene_data$ensembl_id, columns = c("ENSEMBL", "ENTREZID"), keytype = "ENSEMBL")
+    entrez_ids <- unique(entrez_mapping$ENTREZID)
+    
+    ##Perform Pathway Enrichment Analysis (GO/KEGG)
+    
+    ### Perform Gene Ontology (GO) enrichment analysis
+    go_results <- enrichGO(
+      gene = genes_list_go,
+      OrgDb = org.Hs.eg.db,  # Human genome annotations
+      keyType = "SYMBOL",  # Or use "ENSEMBL" depending on your input
+      ont = "BP",  # Biological process; change to "MF" or "CC" for molecular function or cellular component
+      pvalueCutoff = 0.05
+    )
+    
+    go_results_df <- organize_enrichment_results(go_results)
+    go_results_file <- "go_results_allele_freq"
+    if(!is.null(go_results_df)) {
+      write.csv(go_results_df, paste0(go_results_file, ".csv"), quote = FALSE, row.names = FALSE)
+      
+      ##Visualize the enrichment results
+      title_go <- paste0("Top enriched GO terms")
+      ### Bar plot for GO enrichment results
+      png(paste0(saving_dir, "barplot_", go_results_file, ".png"), width = 1920, height = 1080, res = 150)
+      print(barplot(go_results, showCategory = 10, title = title_go))  # Top 20 enriched GO terms
+      dev.off()
+      
+      ### Dot plot for GO enrichment results
+      png(paste0(saving_dir, "dotplot_", go_results_file, ".png"), width = 1920, height = 1080, res = 150)
+      print(dotplot(go_results, showCategory = 10, title = title_go))
+      dev.off()
+    } else {
+      print(paste0("GO: No significant results"))
+    }
+    
+    ### Perform KEGG pathway enrichment analysis
+    if(!is.null(entrez_ids) && length(entrez_ids) != 0) {
+      kegg_results <- enrichKEGG(
+        gene = entrez_ids,
+        organism = "hsa",  # KEGG code for human
+        pvalueCutoff = 0.05
+      )
+      
+      kegg_results_df <- organize_enrichment_results(kegg_results)
+      kegg_results_file <- "kegg_results_allele_freq"
+      if(!is.null(kegg_results_df)) {
+        write.csv(kegg_results_df, paste0(kegg_results_file, ".csv"), quote = FALSE, row.names = FALSE)
+        ##Visualize the enrichment results
+        
+        title_kegg <- paste0("Enriched KEGG pathways")
+        
+        
+        ### Bar plot for KEGG pathway enrichment results
+        png(paste0(saving_dir, "barplot_", kegg_results_file, ".png"), width = 1920, height = 1080, res = 150)
+        print(barplot(kegg_results, showCategory = 10, title = title_kegg))  # Top 20 enriched KEGG pathways
+        dev.off()
+        ### Dot plot for KEGG enrichment results
+        png(paste0(saving_dir, "dotplot_", kegg_results_file, ".png"), width = 1920, height = 1080, res = 150)
+        print(dotplot(kegg_results, showCategory = 10, title = title_kegg))
+        dev.off()
+      } else {
+        print(paste0("KEGG: No significant results"))
+      }
+    }
+    else {
+      print(paste0("KEGG: No entrez ids"))
+    } 
+  } else {
+    print(paste0("No overlapping genes"))
+  } 
